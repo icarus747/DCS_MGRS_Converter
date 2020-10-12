747a078b5a1a734ff2a136477704c811abaab69f
icarus747 <helperpsp@gmail.com>
2020-10-12 09:22:32 -0500

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+import mgrs
+
+
+def main():
+    m = mgrs.MGRS()
+    # dcs = '38TLN046623'
+    dcs = input("Enter MGRS cord.")
+
+    dd = m.toLatLon(dcs)
+
+    lat = m.ddtodms(dd[0])
+    Lat = round(lat[1] + lat[2] / 60, 1)
+
+    long = m.ddtodms(dd[1])
+    Long = round(long[1] + long[2] / 60, 1)
+
+    print(f"N{int(lat[0])} {Lat}")
+    print(f"E0{int(long[0])} {Long}")
+
+
+if __name__ == '__main__':
+    main()
+    # TODO: Error handeling for incorect MGRS grid.
